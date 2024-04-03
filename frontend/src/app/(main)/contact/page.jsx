cb1b44412208bff0fe530b892aec9180c4a4b1fa
Reenakaushal <reenakaushal663@gmail.com>
2024-04-03 17:43:40 +0530

    contact

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,216 +1,276 @@
-"use client";
 import React from 'react'
 
-const Contact = () => {
+const contact = () => {
   return (
-    import Container from "@/components/container";
-    import { useState } from "react";
-    import { useForm } from "react-hook-form";
-    import useWeb3Forms from "@web3forms/react";
-    import {
-      MapPinIcon,
-      EnvelopeIcon,
-      PhoneIcon
-    } from "@heroicons/react/24/outline";
-    export default function Contact({ settings }) {
-      const {
-        register,
-        handleSubmit,
-        reset,
-        watch,
-        control,
-        setValue,
-        formState: { errors, isSubmitSuccessful, isSubmitting }
-      } = useForm({
-        mode: "onTouched"
-      });
-      const [isSuccess, setIsSuccess] = useState(false);
-      const [message, setMessage] = useState(false);
-      // Please update the Access Key in the Sanity CMS - Site Congig Page
-      const apiKey = settings?.w3ckey || "YOUR_ACCESS_KEY_HERE";
-    
-      const { submit: onSubmit } = useWeb3Forms({
-        access_key: apiKey,
-        settings: {
-          from_name: "Stablo Template",
-          subject: "New Contact Message from Stablo Website"
-        },
-        onSuccess: (msg, data) => {
-          setIsSuccess(true);
-          setMessage(msg);
-          reset();
-        },
-        onError: (msg, data) => {
-          setIsSuccess(false);
-          setMessage(msg);
-        }
-      });
-    
-      return (
-        <Container>
-          <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-            Contact
-          </h1>
-          <div className="text-center">
-            <p className="text-lg">We are a here to help.</p>
-          </div>
-    
-          <div className="grid my-10 md:grid-cols-2">
-            <div className="my-10">
-              <h2 className="text-2xl font-semibold dark:text-white">
-                Contact Stablo
-              </h2>
-              <p className="max-w-sm mt-5">
-                Have something to say? We are here to help. Fill up the
-                form or send email or call phone.
-              </p>
-    
-              <div className="mt-5">
-                <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-                  <MapPinIcon className="w-4 h-4" />
-                  <span>1734 Sanfransico, CA 93063</span>
-                </div>
-                {settings?.email && (
-                  <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-                    <EnvelopeIcon className="w-4 h-4" />
-                    <a href={`mailto:${settings.email}`}>
-                      {settings.email}
-                    </a>
-                  </div>
-                )}
-                {settings?.phone && (
-                  <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-                    <PhoneIcon className="w-4 h-4" />
-                    <a href={`tel:${settings.phone}`}>{settings.phone}</a>
-                  </div>
-                )}
+    <>
+  {/* Contact Us */}
+  <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <div className="max-w-xl mx-auto">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-800 sm:text-4xl dark:text-white">
+          Contact us
+        </h1>
+        <p className="mt-1 text-gray-600 dark:text-gray-400">
+          We'd love to talk about how we can help you.
+        </p>
+      </div>
+    </div>
+    <div className="mt-12 max-w-lg mx-auto">
+      {/* Card */}
+      <div className="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8 dark:border-gray-700">
+        <h2 className="mb-8 text-xl font-semibold text-gray-800 dark:text-gray-200">
+          Fill in the form
+        </h2>
+        <form>
+          <div className="grid gap-4 lg:gap-6">
+            {/* Grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+              <div>
+                <label
+                  htmlFor="hs-firstname-contacts-1"
+                  className="block mb-2 text-sm text-gray-700 font-medium dark:text-white"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="hs-firstname-contacts-1"
+                  id="hs-firstname-contacts-1"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="hs-lastname-contacts-1"
+                  className="block mb-2 text-sm text-gray-700 font-medium dark:text-white"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="hs-lastname-contacts-1"
+                  id="hs-lastname-contacts-1"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                />
               </div>
             </div>
-            <div>
-              <form onSubmit={handleSubmit(onSubmit)} className="my-10">
+            {/* End Grid */}
+            {/* Grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+              <div>
+                <label
+                  htmlFor="hs-email-contacts-1"
+                  className="block mb-2 text-sm text-gray-700 font-medium dark:text-white"
+                >
+                  Email
+                </label>
                 <input
-                  type="checkbox"
-                  id=""
-                  className="hidden"
-                  style={{ display: "none" }}
-                  {...register("botcheck")}></input>
-    
-                <div className="mb-5">
-                  <input
-                    type="text"
-                    placeholder="Full Name"
-                    autoComplete="false"
-                    className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-                      errors.name
-                        ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                        : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                    }`}
-                    {...register("name", {
-                      required: "Full name is required",
-                      maxLength: 80
-                    })}
-                  />
-                  {errors.name && (
-                    <div className="mt-1 text-red-600">
-                      <small>{errors.name.message}</small>
-                    </div>
-                  )}
-                </div>
-    
-                <div className="mb-5">
-                  <label htmlFor="email_address" className="sr-only">
-                    Email Address
-                  </label>
-                  <input
-                    id="email_address"
-                    type="email"
-                    placeholder="Email Address"
-                    name="email"
-                    autoComplete="false"
-                    className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
-                      errors.email
-                        ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                        : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                    }`}
-                    {...register("email", {
-                      required: "Enter your email",
-                      pattern: {
-                        value: /^\S+@\S+$/i,
-                        message: "Please enter a valid email"
-                      }
-                    })}
-                  />
-                  {errors.email && (
-                    <div className="mt-1 text-red-600">
-                      <small>{errors.email.message}</small>
-                    </div>
-                  )}
-                </div>
-    
-                <div className="mb-3">
-                  <textarea
-                    name="message"
-                    placeholder="Your Message"
-                    className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
-                      errors.message
-                        ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                        : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                    }`}
-                    {...register("message", {
-                      required: "Enter your Message"
-                    })}
-                  />
-                  {errors.message && (
-                    <div className="mt-1 text-red-600">
-                      {" "}
-                      <small>{errors.message.message}</small>
-                    </div>
-                  )}
-                </div>
-    
-                <button
-                  type="submit"
-                  className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
-                  {isSubmitting ? (
-                    <svg
-                      className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24">
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                  ) : (
-                    "Send Message"
-                  )}
-                </button>
-              </form>
-    
-              {isSubmitSuccessful && isSuccess && (
-                <div className="mt-3 text-sm text-center text-green-500">
-                  {message || "Success. Message sent successfully"}
-                </div>
-              )}
-              {isSubmitSuccessful && !isSuccess && (
-                <div className="mt-3 text-sm text-center text-red-500">
-                  {message || "Something went wrong. Please try later."}
-                </div>
-              )}
+                  type="email"
+                  name="hs-email-contacts-1"
+                  id="hs-email-contacts-1"
+                  autoComplete="email"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="hs-phone-number-1"
+                  className="block mb-2 text-sm text-gray-700 font-medium dark:text-white"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  name="hs-phone-number-1"
+                  id="hs-phone-number-1"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                />
+              </div>
+            </div>
+            {/* End Grid */}
+            <div>
+              <label
+                htmlFor="hs-about-contacts-1"
+                className="block mb-2 text-sm text-gray-700 font-medium dark:text-white"
+              >
+                Details
+              </label>
+              <textarea
+                id="hs-about-contacts-1"
+                name="hs-about-contacts-1"
+                rows={4}
+                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                defaultValue={""}
+              />
             </div>
           </div>
-        </Container>
-      );
-    }
-  )
+          {/* End Grid */}
+          <div className="mt-6 grid">
+            <button
+              type="submit"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+              Send inquiry
+            </button>
+          </div>
+          <div className="mt-3 text-center">
+            <p className="text-sm text-gray-500">
+              We'll get back to you in 1-2 business days.
+            </p>
+          </div>
+        </form>
+      </div>
+      {/* End Card */}
+    </div>
+    <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 items-center gap-4 lg:gap-8">
+      {/* Icon Block */}
+      <a
+        className="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 p-4 sm:p-6 dark:hover:bg-white/[.05] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        href="#"
+      >
+        <svg
+          className="size-9 text-gray-800 mx-auto dark:text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx={12} cy={12} r={10} />
+          <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+          <path d="M12 17h.01" />
+        </svg>
+        <div className="mt-5">
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+            Knowledgebase
+          </h3>
+          <p className="mt-1 text-gray-500">
+            We're here to help with any questions or code.
+          </p>
+          <p className="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600 dark:text-blue-500">
+            Contact support
+            <svg
+              className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </p>
+        </div>
+      </a>
+      {/* End Icon Block */}
+      {/* Icon Block */}
+      <a
+        className="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 p-4 sm:p-6 dark:hover:bg-white/[.05] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        href="#"
+      >
+        <svg
+          className="size-9 text-gray-800 mx-auto dark:text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z" />
+          <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
+        </svg>
+        <div className="mt-5">
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+            FAQ
+          </h3>
+          <p className="mt-1 text-gray-500">
+            Search our FAQ for answers to anything you might ask.
+          </p>
+          <p className="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600 dark:text-blue-500">
+            Visit FAQ
+            <svg
+              className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </p>
+        </div>
+      </a>
+      {/* End Icon Block */}
+      {/* Icon Block */}
+      <a
+        className="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 p-4 sm:p-6 dark:hover:bg-white/[.05] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        href="#"
+      >
+        <svg
+          className="size-9 text-gray-800 mx-auto dark:text-gray-200"
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="m7 11 2-2-2-2" />
+          <path d="M11 13h4" />
+          <rect width={18} height={18} x={3} y={3} rx={2} ry={2} />
+        </svg>
+        <div className="mt-5">
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+            Developer APIs
+          </h3>
+          <p className="mt-1 text-gray-500">
+            Check out our development quickstart guide.
+          </p>
+          <p className="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600 dark:text-blue-500">
+            Contact sales
+            <svg
+              className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </p>
+        </div>
+      </a>
+      {/* End Icon Block */}
+    </div>
+  </div>
+  {/* End Contact Us */}
+</>
+ )
 }
 
-export default Contact
+export default contact
