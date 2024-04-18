64f8e72b754fd0d05f46b86bdc56799243447afc
m-triple-m <triplem656@gmail.com>
2024-04-18 18:10:12 +0530

    fixed add package

--- frontend/src/app/admin/add-package/page.jsx
+++ frontend/src/app/admin/add-package/page.jsx
@@ -36,118 +36,137 @@ const AddPackage = () => {
     }
 
   })
+
+  const uploadFile = (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('myfile', file);
+    fetch('http://localhost:5000/util/uploadfile', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        packageForm.setFieldValue('placeCover', file.name);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
-    <form className="max-w-sm mx-auto" onSubmit={packageForm.handleSubmit}>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-     Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          onChange={packageForm.handleChange}
-          value={packageForm.values.name}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
-          required=""
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Place
-        </label>
-        <input
-          type="text"
-          id="place"
-          onChange={packageForm.handleChange}
-          value= {packageForm.values.place}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required=""
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="repeat-password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Placecover
-        </label>
-        <input
-          type="text"
-          id="Placecover"
-          onChange={packageForm.handleChange}
-          value={packageForm.values.Placecover}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required=" "
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-         Price
-        </label>
-        <input
-          type="text"
-          id="price"
-          onChange={packageForm.handleChange}
-          value={packageForm.values.price}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
-          required=""
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          facilities
-        </label>
-        <input
-          type="text"
-          id="facilities"
-          onChange={packageForm.handleChange}
-          value={packageForm.values.facilities}
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
-          required=""
-        />
-      </div>
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
+    <div className="dark:bg-slate-700">
+
+      <form className="max-w-sm mx-auto" onSubmit={packageForm.handleSubmit}>
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Name
+          </label>
           <input
-            id="terms"
-            type="checkbox"
-            defaultValue=""
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+            type="text"
+            id="name"
+            onChange={packageForm.handleChange}
+            value={packageForm.values.name}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
             required=""
           />
         </div>
-        <label
-          htmlFor="terms"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Place
+          </label>
+          <input
+            type="text"
+            id="place"
+            onChange={packageForm.handleChange}
+            value={packageForm.values.place}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required=""
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="repeat-password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Placecover
+          </label>
+          <input
+            type="file"
+            id="Placecover"
+            onChange={uploadFile}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required=" "
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Price
+          </label>
+          <input
+            type="text"
+            id="price"
+            onChange={packageForm.handleChange}
+            value={packageForm.values.price}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            placeholder="name@flowbite.com"
+            required=""
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            facilities
+          </label>
+          <textarea
+            type="text"
+            id="facilities"
+            onChange={packageForm.handleChange}
+            value={packageForm.values.facilities}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required=""
+          ></textarea>
+        </div>
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="terms"
+              type="checkbox"
+              defaultValue=""
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              required=""
+            />
+          </div>
+          <label
+            htmlFor="terms"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            I agree with the{" "}
+            <a href="agree" className="text-blue-600 hover:underline dark:text-blue-500">
+              terms and conditions
+            </a>
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          I agree with the{" "}
-          <a href="agree" className="text-blue-600 hover:underline dark:text-blue-500">
-            terms and conditions
-          </a>
-        </label>
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Register new account
-      </button>
-    </form>
+          Register new account
+        </button>
+      </form>
+    </div>
 
   )
 }
